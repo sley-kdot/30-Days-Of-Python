f6eb2dbf57035e387637017c5c0e4dd7291dac94
Nnamdi Okonmah <nnamdiokonma@gmail.com>
2026-01-12 00:44:21 +0100

    Day 1: Completed exercise

---
+++ 01_Day_Introduction/euclidean_distance.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import math
+
+# Find an Euclidean distance between (2, 3) and (10, 8)
+x1, y1 = 2, 3 
+x2, y2 = 10, 8
+
+distance = math.sqrt(((10 - 2)**2) + ((8 - 3)**2))
+print("The distance is: ",distance)
---
+++ 01_Day_Introduction/helloworld.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import platform
+
+#1. Check the python version you are using
+print("version of python", platform.python_version())
+print()
+
+#2. Compute the following operands 3 and 4.
+print("3 + 4 = ", 3+4)
+print("3 - 4 = ", 3-4)
+print("3 * 4 = ", 3*4)
+print("3 % 4 = ", 3%4)
+print("3 / 4 = ", 3/4)
+print("3 ** 4 = ", 3**4)
+print("3 // 4 = ", 3//4)
+print()
+
+#3. Execute the following strings
+print("Nnamdi")
+print("Okonmah")
+print("Nigeria")
+print("I am enjoying 30 days of python")
+print()
+
+#4. Check the data types of the following
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(["Asabeneh", "Python", "Finland"]))
+print(type("Nnamdi"))
+print(type("Okonmah"))
+print(type("Nigeria"))
+
