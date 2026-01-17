50491eef38b7186c02512008ee2ff0bee64a68d6
Nnamdi Okonmah <nnamdiokonma@gmail.com>
2026-01-17 23:13:01 +0100

    Day 4: learning how to use string and it's various methods

---
+++ 04_Day_Strings/strings.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+words = ["Thirty", "Days", "Of", "Python"]
+result = "".join(words)
+print(result)
+for_all = ["Coding", "For", "All"]
+result = "".join(for_all)
+
+company = "Coding For All"
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+print(company[1:])
+print(company.index("Coding"))
+print(company.replace("Coding", "Maths"))
+print(company.split(" "))
+tech_company = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(tech_company.split(","))
+print(company[0])
+print(len(company) - 1)
+print(company[10])
+words = "Python For Everyone"
+print(company.index("C"))
+print(company.index("F"))
+print(company.rindex("l"))
+sentence = 'You cannot end a sentence with because because because is a conjunction'
+print(sentence.find("because"))
+print(sentence[31:54])
+print(company.startswith("Coding"))
+print(company.startswith("coding"))
+my_list = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+print("# ".join(my_list))
+print("I am enjoying this challenge\nI just wonder what is next")
+print("Name\tAge\tCountry\tCity\nNigeria\t25\tNigeria\tKaduna")
+
