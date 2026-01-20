577b594bfe9da37e3ecf7b81abe12bde5e9c0878
Nnamdi Okonmah <nnamdiokonma@gmail.com>
2026-01-20 17:28:43 +0100

    Day 6: played around with tuples

---
+++ 06_Day_Tuples/tuples.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+my_tuple = tuple()
+
+fem_sibs = ("Jane", "Joan")
+male_sibs = ("John", "Jimmy")
+sibs = male_sibs + fem_sibs
+print(sibs)
+print(len(sibs))
+family_mem = ("John", "Jimmy", "Jane", "Joan", "Dad", "Mom")
