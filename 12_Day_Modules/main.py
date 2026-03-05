4a50a6e49170ddcd2974c24b173bae8b339fc63a
Nnamdi Okonmah <nnamdiokonma@gmail.com>
2026-03-05 10:30:33 +0100

    Day 12: complete exercise 1 on modules

--- 12_Day_Modules/main.py
+++ 12_Day_Modules/main.py
@@ -39,3 +39,51 @@ def rgb_color_gen():
     return f"rbg({red},{green},{blue})"
 
 print(rgb_color_gen())
+
+def list_of_hex_colors():
+    character = string.ascii_lowercase
+    alphanum = character[0:6] + string.digits
+    
+    hexa_list = []
+    
+    for i in range(5):
+        random_str = "#"
+        for j in range(6):
+            random_chars = random.choice(alphanum)
+            random_str += random_chars
+        hexa_list.append(random_str)
+    return hexa_list
+print(list_of_hex_colors())
+
+def list_of_rgb_colors():
+    rgb_list = []
+    for i in range(6):
+        rgb_list.append(rgb_color_gen())
+    return rgb_list
+print(list_of_rgb_colors())
+
+def generate_colors(color_type, number):
+    red = random.randint(0, 255)
+    green = random.randint(0, 255)
+    blue = random.randint(0, 255)
+    character = string.ascii_lowercase
+    alphanum = character[0:6] + string.digits
+
+    result = []
+    
+    if color_type == "rgb":
+        for i in range(number):
+            result.append(f"rgb({red},{green},{blue})")
+    elif color_type == "hexa":
+        for i in range(number):
+            random_str = "#"
+            for j in range(6):
+                random_chars = random.choice(alphanum)
+                random_str += random_chars
+            result.append(random_str)
+    return result
+
+print(generate_colors('hexa', 3))
+print(generate_colors('hexa', 1))
+print(generate_colors('rgb', 3))  
+print(generate_colors('rgb', 1))
